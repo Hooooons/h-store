67e9e6e728fc556efe0cf3c9bfd3cbecddbd2561
Hooooons <59018625+Hooooons@users.noreply.github.com>
2021-02-09 17:09:33 +0900

    파이어베이스 서버 연동 및 라이브러리 설치
    firebase, react-icons, react-hook-form,
    antd

--- src/components/LoginPage/SignUp.js
+++ src/components/LoginPage/SignUp.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function SignUp() {
-  return <div>sign up</div>;
+  const { register, watch, errors } = useForm();
+
+  return (
+    <div>
+      <form>
+        <label>Example</label>
+        <input name="example" defaultValue="test" />
+        <label>ExampleRequired</label>
+        <input
+          name="exampleRequired"
+          // ref={register({ required: true, maxLength: 10 })}
+        />
+        {/* {errors.exampleRequired && <p>This field is required</p>} */}
+        <input type="submit" />
+        <Link style={{ color: "gray", textDecoration: "none" }} to="login">
+          이미 아이디가 있다면..
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./redux";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
